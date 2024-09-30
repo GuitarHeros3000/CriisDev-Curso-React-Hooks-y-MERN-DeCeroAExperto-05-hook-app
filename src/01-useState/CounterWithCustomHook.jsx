97077cb2034c3f070cdceb5463d7585d828b6e3d
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-30 14:54:08 -0600

    Exponer métodos del Hook.

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -2,7 +2,7 @@ import { useCounter } from "../hooks/useCounter"
 
 export const CounterWithCustomHook = () => {
 
-    const { counter } = useCounter();
+    const { counter, increment, decrement, reset } = useCounter();
 
     return (
         <>
@@ -12,14 +12,17 @@ export const CounterWithCustomHook = () => {
 
             <button
                 className="btn btn-primary"
+                onClick={() => increment(2)}
             >+1</button>
 
             <button
                 className="btn btn-primary"
+                onClick={reset}
             >Reset</button>
 
             <button
                 className="btn btn-primary"
+                onClick={() => decrement(2)}
             >-1</button>
 
         </>
