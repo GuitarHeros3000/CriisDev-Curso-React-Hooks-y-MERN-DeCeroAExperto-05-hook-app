100c4500fed8be46b7be354593c98d39598fb3dc
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-10-02 15:12:15 -0600

    useEffect - Precauciones.

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,12 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 })
+
     useEffect(() => {
-        console.log('Message Mounted');
+
+        const onMouseMOve = ({ x, y }) => {
+            // const coords = { x, y };
+            setCoords({ x, y });
+        };
+
+        window.addEventListener('mousemove', onMouseMOve);
 
         return () => {
-            console.log('Message Unmounted');
+            window.removeEventListener('mousemove', onMouseMOve);
         }
     }, []);
 
@@ -14,6 +22,7 @@ export const Message = () => {
     return (
         <>
             <h3>Usuario ya existe</h3>
+            {JSON.stringify(coords)}
         </>
     )
 }
